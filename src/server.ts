cdf173a7a68157e77e74c8c6cdf4f7c1160ba690
cooperrunyan <cooperrunyanspam@gmail.com>
2022-06-29 19:30:43 -0700

    Add test for app

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,7 @@
-import express from 'express';
 import { config } from 'dotenv';
+import { app } from './app/app.js';
 
-const app = express();
 config();
 
-app.use(express.json());
-app.get('/ping', async (req, res) => res.send('pong'));
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
